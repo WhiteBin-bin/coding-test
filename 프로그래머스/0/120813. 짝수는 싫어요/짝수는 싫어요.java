13d03dd9590a4d522850376bb2461ea7240210ba
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-11-07 16:37:08 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int n) {
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                count++;
+            }
+        }
+        int[] answer = new int[count];
+        // 1. answer 배열의 인덱스를 관리할 변수 추가
+        int idx = 0; 
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 != 0) {
+                // 2. i가 아닌 idx를 인덱스로 사용
+                answer[idx] = i;
+                // 3. 인덱스를 1 증가시켜 다음 칸을 가리키게 함
+                idx++; 
+            }
+        }
+        return answer;
+    }
+}
