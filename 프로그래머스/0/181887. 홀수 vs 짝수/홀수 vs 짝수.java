a4ab40a5160ec6819e59d3b44b9c6da52bfcad69
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-13 22:47:32 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        
+        int odd = 0;
+        int even = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            
+            if ((i + 1) % 2 == 0) {
+                even += num_list[i];
+            } else if ((i + 1) % 2 == 1) {
+                odd += num_list[i];
+            }
+        }
+        
+        return odd > even ? odd : even;
+    }
+}
