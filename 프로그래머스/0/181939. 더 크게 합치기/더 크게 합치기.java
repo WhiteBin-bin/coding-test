b4cff3f26186e1050ba6d491dd51e3aca4a8b968
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-13 15:08:41 +0900

    [level 0] Title: 더 크게 합치기, Time: 2.07 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int ba = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+        
+        if (ab > ba) {
+            return ab;
+        } else if (ab == ba) {
+            return ab;
+        } else {
+            return ba;
+        }
+    }
+}
+
+        
