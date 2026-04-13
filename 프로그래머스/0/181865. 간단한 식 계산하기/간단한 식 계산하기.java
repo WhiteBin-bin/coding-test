02678a76b3e1199cf86dac48241b7bc8416b6469
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-13 14:32:14 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String binomial) {
+        
+        String[] str = binomial.split(" ");
+        
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[2]);
+        String op = str[1];
+        
+        if (op.equals("+")) {
+            return a + b;
+        } else if (op.equals("-")) {
+            return a - b;
+        } else if (op.equals("*")) {
+            return a * b;
+        }
+        
+        return 0;
+    }
+}
