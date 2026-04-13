a16591b5679de009eb5cc105f8e8f26124c64b25
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-13 15:15:05 +0900

    [level 2] Title: 전화번호 목록, Time: 257.49 ms, Memory: 270 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
+        
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++) {
+                String prefix = phone.substring(0, i);
+                
+                if (set.contains(prefix)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
