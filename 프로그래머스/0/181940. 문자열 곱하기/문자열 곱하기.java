97bb5bad0ee9be9cb6925e47ab311d98209d0834
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-13 14:06:02 +0900

    [level 0] Title: 문자열 곱하기, Time: 2.44 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        
+        String str = "";
+        for(int i = 0; i < k; i++) {
+            str += my_string;
+        }
+        
+        return str;
+    }
+}
