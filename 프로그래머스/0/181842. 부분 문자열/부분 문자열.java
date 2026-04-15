8f603e447058e1ca6aeeab717e4f49a9351eeeb7
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-15 16:45:15 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int num = 0;
+        if(str2.contains(str1)){
+            num = 1;
+        }
+        return num;
+    }
+}
