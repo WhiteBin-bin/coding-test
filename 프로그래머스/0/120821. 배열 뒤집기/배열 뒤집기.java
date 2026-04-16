cc83819d66630299be45738dc9fc76e73d0aa540
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-16 15:04:06 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.17 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,10 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list) {
         
+        Stack<Integer> stack = new Stack<>();
         int[] arr = new int[num_list.length];
         
-        for (int i = 0; i < num_list.length; i++) {
-            arr[i] = num_list[num_list.length - 1 - i];
+        for(int num : num_list) {
+            stack.push(num);
+        }
+        
+        while(!stack.isEmpty()) {
+            
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = stack.pop();
+            }
         }
         
         return arr;
