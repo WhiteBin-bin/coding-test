cac773311d197721bf9407c5830792c0c3b4c29f
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-20 11:31:51 +0900

    [level 0] Title: 그림 확대, Time: 0.39 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        
+        String[] arr = new String[picture.length * k];
+        int index = 0;
+        for (int i = 0; i < picture.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < picture[i].length(); j++) {
+                for (int n = 0; n < k; n++) {
+                    sb.append(picture[i].charAt(j));
+                }
+            }   
+            for (int j = 0; j < k; j++) {
+                arr[index++] = sb.toString();
+            }
+        }
+        return arr;
+    }
+}
