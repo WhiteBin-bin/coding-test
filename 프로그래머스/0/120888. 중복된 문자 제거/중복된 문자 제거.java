5a372511a053ad5ef38e91e014c7d88abe9b3e54
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-04-24 15:52:30 +0900

    [level 0] Title: 중복된 문자 제거, Time: 12.67 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        String str = "";
+                
+        for(int i = 0; i < my_string.length(); i++) {
+            if(my_string.indexOf(my_string.charAt(i)) == i) {
+                str += my_string.charAt(i);
+            }
+        }
+        return str;
+    }
+}
