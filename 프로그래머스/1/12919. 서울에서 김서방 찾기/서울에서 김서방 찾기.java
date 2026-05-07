047c33ff2a1d7bdd8dd4cf7915e1362d12db16af
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-05-07 15:42:10 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.31 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] seoul) {
+        
+        int index = 0;
+        
+        for (String str : seoul) {
+            if (str.equals("Kim")) {
+                break;
+            }
+            
+            index++;
+        }
+        
+        return String.format("김서방은 %d에 있다", index);
+    }
+}
