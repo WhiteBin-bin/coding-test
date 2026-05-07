8d599f584481606bb3acfdef15410616ec1d1492
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-05-07 16:15:51 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 30.52 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+
+        String str = "";
+        
+	    for (int i = 1; i <= n; i++) {
+    	
+            str += i % 2 != 0 ? "수" : "박";
+        }
+ 
+        return str;
+    }
+}
