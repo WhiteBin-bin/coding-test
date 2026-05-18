d7e3200cb2425d103f6d88f921f1aa675f885503
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-05-18 16:05:09 +0900

    Time: 374 ms (14.96%), Space: 47.2 MB (24.6%) - LeetHub

---
+++ LeetCode/Easy/2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+
+    Set<Integer> set = new HashSet<>();
+
+    public int[] findEvenNumbers(int[] digits) {
+
+        boolean[] visited = new boolean[digits.length];
+
+        dfs(digits, visited, 0, 0);
+
+        return set.stream()
+                .sorted()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    private void dfs(int[] digits, boolean[] visited, int depth, int num) {
+
+        if (depth == 3) {
+
+            if (num % 2 == 0) {
+                set.add(num);
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+
+            if (visited[i]) {
+                continue;
+            }
+
+            if (depth == 0 && digits[i] == 0) {
+                continue;
+            }
+
+            visited[i] = true;
+
+            dfs(digits, visited, depth + 1, num * 10 + digits[i]);
+
+            visited[i] = false;
+        }
+    }
+}
