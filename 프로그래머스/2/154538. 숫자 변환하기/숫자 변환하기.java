280fa684b48dc89ecb6f11fc5c0ee7da34468b59
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-07-03 15:43:18 +0900

    [level 2] Title: 숫자 변환하기, Time: 17.19 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int x, int y, int n) {
+        int INF = 1_000_001;
+        int[] dp = new int[y + 1];
+
+        Arrays.fill(dp, INF);
+        dp[x] = 0;
+
+        for (int i = x; i <= y; i++) {
+            if (dp[i] == INF) continue;
+
+            if (i + n <= y) {
+                dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+            }
+
+            if (i * 2 <= y) {
+                dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+            }
+
+            if (i * 3 <= y) {
+                dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
+            }
+        }
+
+        return dp[y] == INF ? -1 : dp[y];
+    }
+}
