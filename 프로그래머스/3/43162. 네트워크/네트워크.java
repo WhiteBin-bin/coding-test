fb05db119d72ab1f4b01b4aaaaf72d5dc405264a
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-08-22 15:53:22 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,26 @@
 class Solution {
-    
-    boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
         
-        visited = new boolean[n];
-        
-        int answer = 0;
+        int count = 0;
+        boolean[] visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i, n, computers);
-                answer++;
-            }
-        }
-        
-        return answer;
-    }
-    
-    private void dfs(int current, int n, int[][] computers) {
-        visited[current] = true;
-        
-        for (int next = 0; next < n; next++) {
-            
-            if (current == next) {
+            if (visited[i]) {
                 continue;
             }
-            
-            if (computers[current][next] == 1 && !visited[next]) {
-                dfs(next, n, computers);
+            dfs(n, computers, visited, i);
+            count++;
+        }
+        
+        return count;
+    }
+    
+    void dfs(int n, int[][] computers, boolean[] visited, int cur) {
+        visited[cur] = true;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && computers[cur][i] == 1) {
+                dfs(n, computers, visited, i);
             }
         }
     }
